788bc3811587bce0f82cc93e698ed13202569568
Vance Sereda <vancesereda@gmail.com>
2019-04-22 21:19:41 -0700

    formatting

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api', async (req,res) => {
+app.get('/api', async (req,res, next) => {
 
     if (req.query) {
         getVideo(req.query.id)
-        .then(data=> {
-            res.send(JSON.stringify(data.response, null, 2))
-            .catch(e=>res.send({'Error': 'Probably an unlisted video, try again'}))
-        })
+        .then(data=> res.send(res=> JSON.stringify(data.response, null, 2)))
+        .catch(e=>res.send({'Error': 'Probably an unlisted video, try again'}))
+        
     }   
     // res.send(JSON.stringify(getVideo(req.params.id)))
 })
